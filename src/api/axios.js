4c5fe69aba7299ef956f28f2c168eab80d2d0b8b
arthur <arthurcarvalho24@gmail.com>
2020-01-30 16:27:26 -0300

    removed gitignore files

--- src/api/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({baseURL: 'http://gteste.alivepro.com.br:8080/api'})
-// const api = axios.create({baseURL: 'http://localhost:8080/api'})
-
-export default api
--- src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "reducers/authReducer";
-import statCardReducer from "reducers/statCardReducer";
-export default combineReducers({
-  // the authReducer will work only with authState
-  authState: authReducer,
-  // the statCardReducer will work only with statCardState
-  statCardState: statCardReducer
-});
